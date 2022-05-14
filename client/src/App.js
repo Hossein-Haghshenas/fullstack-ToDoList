d31236c6b1fe48eb327203e5e263293860bd45bd
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-05-14 13:32:23 +0430

    Create state and addtodo function

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import Container from "./components/Container";
 import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState([
+    {
+      id: nanoid(),
+      text: "first work",
+    },
+  ]);
+
+  const newTodo = (todoText) => {
+    const setNewTodo = {
+      id: nanoid(),
+      text: todoText,
+    };
+
+    setTodo([...todo, setNewTodo]);
+  };
+
   return (
     <>
-      <Container />
+      <Container todoList={todo} handleNewTodo={newTodo} />
     </>
   );
 }
