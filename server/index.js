525ac2f12433c1bc20bcfd2aa7861aa98e84eefa
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-05-24 15:05:57 +0430

    connect to mongoDB

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./db/connect");
+const todo = require("./routes/todo");
+const cors = require("cors");
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", todo);
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+
+    app.listen(PORT, () => {
+      console.log(`server is up on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
