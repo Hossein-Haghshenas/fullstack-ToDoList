d3b556edcbec3ce6a4982a8810078ae068ae6d0c
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-05-25 20:51:00 +0430

    Create delete btn & start btn for make todo bold

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { AiFillDelete, AiFillStar } from "react-icons/ai";
+import { deleteTodo } from "../api/todoApi";
+import UpdateTodo from "./UpdateTodo";
 
 const TodoList = () => {
+  const navigate = useNavigate();
   const [data, setData] = useState([]);
+  const [isBold, setIsBold] = useState(true);
 
   useEffect(() => {
     try {
@@ -10,15 +16,37 @@ const TodoList = () => {
     } catch (error) {}
   }, []);
 
+  const deleteHandler = (id) => {
+    deleteTodo(id);
+    navigate(0);
+  };
+
+  const starHandler = (id) => {
+    setIsBold(!isBold);
+    if (isBold) {
+      document.getElementById(id).style.fontWeight = "700";
+    } else {
+      document.getElementById(id).style.fontWeight = "200";
+    }
+  };
+
   return (
     <section className="todo-list">
       <ul>
         {data?.map((todo) => {
-          const { text, id } = todo;
-
+          const { text, _id } = todo;
           return (
-            <li className="todo-item" key={id}>
-              {text}
+            <li className="todo-item" key={_id}>
+              <p id={_id}>{text}</p>
+              <span>
+                <button className="delete-icon">
+                  <AiFillDelete onClick={() => deleteHandler(_id)} />
+                </button>
+                <UpdateTodo id={_id} text={text} />
+                <button className="star-icon">
+                  <AiFillStar onClick={() => starHandler(_id)} />
+                </button>
+              </span>
             </li>
           );
         })}
