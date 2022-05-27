106f81c78fb913cecf072f1aafc39d3555bbdc48
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-05-27 23:52:23 +0430

    Create history page

---
+++ client/src/components/History.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { BsCheckCircleFill } from "react-icons/bs";
+import axios from "axios";
+
+const History = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    try {
+      axios.get("http://localhost:5000").then((res) => setData(res.data.todos));
+    } catch (error) {}
+  }, []);
+
+  return (
+    <section className="container">
+      <section className="todo-list">
+        <ul>
+          {data?.map((todo) => {
+            const { text, _id, statusColor } = todo;
+            return (
+              statusColor === "#00ff15b7" && (
+                <li
+                  className="todo-item"
+                  key={_id}
+                  style={{ backgroundColor: `${statusColor}` }}
+                >
+                  <p>{text}</p>
+                  <span className="todo-btns">
+                    <BsCheckCircleFill />
+                  </span>
+                </li>
+              )
+            );
+          })}
+        </ul>
+      </section>
+    </section>
+  );
+};
+
+export default History;
