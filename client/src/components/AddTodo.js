8dd7dbe50949db5a58fd810f15ab7056d2b4e1cd
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-05-27 16:51:44 +0430

    Create ui for timer inputs

--- client/src/components/AddTodo.js
+++ client/src/components/AddTodo.js
@@ -7,37 +7,72 @@ import { createTodo } from "../api/todoApi";
 const AddTodo = ({ handleNewTodo }) => {
   const navigate = useNavigate();
   const [newTodoText, setNewTodoText] = useState("");
+  const [timer, setTimer] = useState(false);
+  const [newTodoTime, setNewTodoTime] = useState(null);
 
   const handleClick = (e) => {
     e.preventDefault();
     newTodoText !== ""
-      ? newTodo(newTodoText)
+      ? newTodo(newTodoText, newTodoTime)
       : alert("Please enter something !");
     setNewTodoText("");
   };
 
-  const newTodo = (todoText) => {
+  const newTodo = (todoText, todotime) => {
     const setNewTodo = {
       id: nanoid(),
       text: todoText,
+      time: todotime,
+      date: new Date(),
     };
     createTodo(setNewTodo);
+    setTimer(false);
     navigate(0);
   };
 
+  const hidden = timer ? { visibility: "hidden" } : { visibility: "visible" };
+  const visible = timer ? { visibility: "visible" } : { visibility: "hidden" };
+
   return (
-    <section className="todo-input-container">
+    <section className="todo-container">
       <form>
-        <input
-          type="text"
-          className="todo-input"
-          placeholder="new Todo"
-          value={newTodoText}
-          onChange={(e) => setNewTodoText(e.target.value)}
-        />
-        <button type="submit" className="input-icon" onClick={handleClick}>
-          <AiOutlinePlusCircle />
-        </button>
+        <section className="todo-input-container">
+          <input
+            type="text"
+            className="todo-input"
+            placeholder="new Todo"
+            value={newTodoText}
+            onChange={(e) => setNewTodoText(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="todo-input-btn"
+            onClick={handleClick}
+          >
+            <AiOutlinePlusCircle />
+          </button>
+        </section>
+        <section className="todo-input-container">
+          <section className="check-box-container">
+            <input
+              type="checkbox"
+              className="todo-input-btn timer-checkbox"
+              name="timer"
+              onClick={() => setTimer(!timer)}
+            />
+            <label style={hidden} htmlFor="timer" className=" timer-lable">
+              Todo Timer
+            </label>
+          </section>
+
+          <input
+            type="number"
+            className="todo-input todo-timer-input"
+            placeholder="set your time... "
+            onChange={(e) => setNewTodoTime(e.target.value)}
+            style={visible}
+          />
+        </section>
       </form>
     </section>
   );
