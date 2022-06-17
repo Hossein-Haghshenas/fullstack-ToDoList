221c0491d3f1417c3bea59183d49bd7309548e89
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-06-17 23:36:22 +0430

    Fix something

--- client/src/components/AddTodo.js
+++ client/src/components/AddTodo.js
@@ -1,23 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { nanoid } from "nanoid";
-import { AiOutlinePlusCircle } from "react-icons/ai";
 import { createTodo } from "../api/todoApi";
+import Filters from "./Filters";
+import NewTodoForm from "./NewTodoForm";
 
-const AddTodo = ({ handleNewTodo, statusHandler }) => {
+const AddTodo = ({ statusHandler, searchHandler }) => {
   const navigate = useNavigate();
-  const [newTodoText, setNewTodoText] = useState("");
-  const [timer, setTimer] = useState(false);
-  const [newTodoTime, setNewTodoTime] = useState(null);
   const [callStatus, setCallStatus] = useState(false);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    newTodoText !== ""
-      ? newTodo(newTodoText, newTodoTime)
-      : alert("Please enter something !");
-    setNewTodoText("");
-  };
 
   const newTodo = (todoText, todotime) => {
     const setNewTodo = {
@@ -32,56 +22,17 @@ const AddTodo = ({ handleNewTodo, statusHandler }) => {
 
   if (callStatus === true) {
     statusHandler(callStatus);
+    navigate(0);
     setCallStatus(false);
   } else {
     statusHandler(callStatus);
   }
 
-  const hidden = timer ? { visibility: "hidden" } : { visibility: "visible" };
-  const visible = timer ? { visibility: "visible" } : { visibility: "hidden" };
-
   return (
-    <section className="todo-container">
-      <form>
-        <section className="todo-input-container">
-          <input
-            type="text"
-            className="todo-input"
-            placeholder="new Todo"
-            value={newTodoText}
-            onChange={(e) => setNewTodoText(e.target.value)}
-          />
-          <button
-            type="submit"
-            className="todo-input-btn"
-            onClick={handleClick}
-          >
-            <AiOutlinePlusCircle />
-          </button>
-        </section>
-        <section className="todo-input-container">
-          <section className="check-box-container">
-            <input
-              type="checkbox"
-              className="todo-input-btn timer-checkbox"
-              name="timer"
-              onClick={() => setTimer(!timer)}
-            />
-            <label style={hidden} htmlFor="timer" className=" timer-lable">
-              Todo Timer
-            </label>
-          </section>
-
-          <input
-            type="number"
-            className="todo-input todo-timer-input"
-            placeholder="How much minutes ? "
-            onChange={(e) => setNewTodoTime(e.target.value * 60)}
-            style={visible}
-          />
-        </section>
-      </form>
-    </section>
+    <>
+      <Filters searchHandler={searchHandler} />
+      <NewTodoForm newTodo={newTodo} />
+    </>
   );
 };
 
--- client/src/components/container.js
+++ client/src/components/container.js
@@ -3,23 +3,27 @@ import AddTodo from "./AddTodo";
 import { useState } from "react";
 import { Loading, Grid } from "@nextui-org/react";
 
-const Container = ({ searchText }) => {
+const Container = ({ searchHandler, searchText }) => {
   const [callStatus, setCallStatus] = useState(false);
 
   const statusHandler = (status) => setCallStatus(status);
 
   return (
-    <section className="container">
-      <AddTodo statusHandler={statusHandler} />
-      {callStatus ? (
-        <Grid>
-          <Loading color="secondary" textColor="secondary">
-            Please wait
-          </Loading>
-        </Grid>
-      ) : (
-        <TodoList searchText={searchText} />
-      )}
+    <section className="main-page p-sm-5 p-1">
+      <section className="todo-container flex-sm-nowrap m-auto m-sm-0">
+        <AddTodo statusHandler={statusHandler} searchHandler={searchHandler} />
+      </section>
+      <section>
+        {callStatus ? (
+          <Grid>
+            <Loading color="secondary" textColor="secondary">
+              Please wait
+            </Loading>
+          </Grid>
+        ) : (
+          <TodoList searchText={searchText} />
+        )}
+      </section>
     </section>
   );
 };
