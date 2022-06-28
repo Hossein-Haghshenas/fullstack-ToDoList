f1f7e63cf0e1ebf1b7431474906621d71a50d5c6
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-06-28 23:44:25 +0430

    Create user api

---
+++ client/src/api/userApi.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const baseUrl = "https://mytodolistapp2022.herokuapp.com";
+
+const createUser = (user) => {
+  try {
+    const status = axios
+      .post(`${baseUrl}/register`, user)
+      .then((data) => data.status === 200 && true);
+    return status;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateUser = (id, user, navigate) => {
+  try {
+    axios
+      .patch(`${baseUrl}/user/${id}`, user)
+      .then((data) => data.status === 200 && navigate(0));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteUser = (id, navigate) => {
+  try {
+    axios
+      .delete(`${baseUrl}/admin/${id}`)
+      .then((data) => data.status === 200 && navigate(0));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { createUser, deleteUser, updateUser };
