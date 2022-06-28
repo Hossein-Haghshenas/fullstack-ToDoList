8e10f2d23129e60c2c59d43611bbbaccac3388bb
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-06-28 23:44:07 +0430

    add user in app

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./db/connect");
 const todo = require("./routes/todo");
+const user = require("./routes/user");
 const cors = require("cors");
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", todo);
+app.use("/", user);
 
 const PORT = process.env.PORT || 5000;
 
