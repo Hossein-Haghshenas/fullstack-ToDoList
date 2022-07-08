c1040a04237d0b0f9f957ea169e08aff266de917
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-08 18:46:11 +0430

    Create a component for sometime todo is 0

---
+++ client/src/components/NoTodoPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NoTodoPage({ username }) {
+  return (
+    <section className="text-center">
+      <img
+        className=" w-50"
+        src={require("../image/undraw_Post_re_mtr4-removebg-preview.png")}
+        alt=""
+      />
+      <h2>welcome {username} </h2>
+      <p>Create your first Todo</p>
+    </section>
+  );
+}
+
+export default NoTodoPage;
