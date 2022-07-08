6c60fad35225e8297623ede8fe0065e4d360bef3
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-08 18:46:33 +0430

    Display todo for users

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,23 +1,31 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import jwt_decode from "jwt-decode";
 import { AiFillStar } from "react-icons/ai";
 import UpdateTodo from "./UpdateTodo";
 import DeleteTodo from "./DeleteTodo";
 import Timer from "./Timer";
 import { Loading, Grid, Card, Text, Avatar, Col } from "@nextui-org/react";
+import { getTodo } from "../api/todoApi";
+import NoTodoPage from "./NoTodoPage";
 
 const TodoList = ({ searchText }) => {
   const [data, setData] = useState([]);
   const [isBold, setIsBold] = useState(false);
   const [boldElement, setBoldElement] = useState("");
+  const [status, setStatus] = useState("");
+  const [user, setUser] = useState("");
 
   useEffect(() => {
-    try {
-      axios
-        .get("https://mytodolistapp2022.herokuapp.com/")
-        .then((res) => setData(res.data.todos));
-    } catch (error) {}
-  }, []);
+    const jwtToken = localStorage.getItem("access_key");
+    const userInfo = jwt_decode(jwtToken);
+    setUser(userInfo.username);
+
+    user &&
+      getTodo(user).then((res) => {
+        setStatus(res.status);
+        setData(res.data.todos);
+      });
+  }, [user]);
 
   const starHandler = (id) => {
     setIsBold(!isBold);
@@ -26,7 +34,7 @@ const TodoList = ({ searchText }) => {
 
   return (
     <section className="list-container">
-      {Object.keys(data).length === 0 ? (
+      {status !== 200 ? (
         <Grid className="mt-5">
           <Loading color="secondary" textColor="secondary">
             Please wait
@@ -34,80 +42,86 @@ const TodoList = ({ searchText }) => {
         </Grid>
       ) : (
         <section className="todo-list py-5">
-          <Grid.Container justify="center" className="p-0">
-            {data
-              ?.filter((todo) => todo.text.includes(searchText))
-              .map((todo) => {
-                const { text, _id, time, date, statusColor } = todo;
-                return (
-                  <Grid key={_id}>
-                    <Card
-                      css={{ m: "$8", mw: "280px", h: "15rem" }}
-                      style={{ backgroundColor: `${statusColor}` }}
-                    >
-                      <Card.Header>
-                        <Avatar
-                          size="lg"
-                          src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                          color="gradient"
-                          bordered
-                        />
-                        <Grid.Container css={{ pl: "$6" }}>
-                          <Grid xs={12}>
-                            <Text h4 css={{ lineHeight: "$xs" }}>
-                              practice english
-                            </Text>
-                          </Grid>
-                          <Grid xs={12}>
-                            <Text css={{ color: "$accents8" }}>user</Text>
-                          </Grid>
-                        </Grid.Container>
-                      </Card.Header>
-                      <Card.Body css={{ py: "$2" }}>
-                        <Text>
-                          {_id === boldElement && isBold === true ? (
-                            <b>{text}</b>
-                          ) : (
-                            text
-                          )}
-                        </Text>
-                      </Card.Body>
-                      <Card.Footer
-                        css={{
-                          bgBlur: "#0f111466",
-                          borderTop: "$borderWeights$light solid $gray800",
-                          zIndex: 1,
-                          h: "4rem",
-                        }}
+          {Object.keys(data).length === 0 ? (
+            <>
+              <NoTodoPage username={user} />
+            </>
+          ) : (
+            <Grid.Container justify="center" className="p-0">
+              {data
+                ?.filter((todo) => todo.text.includes(searchText))
+                .map((todo) => {
+                  const { text, _id, time, date, statusColor } = todo;
+                  return (
+                    <Grid key={_id}>
+                      <Card
+                        css={{ m: "$8", mw: "280px", h: "15rem" }}
+                        style={{ backgroundColor: `${statusColor}` }}
                       >
-                        <Col>
-                          <span className="todo-btns">
-                            <DeleteTodo todoId={_id} />
-                            <UpdateTodo todoId={_id} text={text} />
-                            <button
-                              className="star-icon"
-                              onClick={() => starHandler(_id)}
-                            >
-                              <AiFillStar />
-                            </button>
-                          </span>
-                        </Col>
-                        <Col>
-                          {time !== null && (
-                            <Timer
-                              timeValue={time}
-                              dateValue={date}
-                              todoId={_id}
-                              colorHandler={statusColor}
-                            />
-                          )}
-                        </Col>
-                      </Card.Footer>
-                    </Card>
-                  </Grid>
-                );
-              })}
-          </Grid.Container>
+                        <Card.Header>
+                          <Avatar
+                            size="lg"
+                            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                            color="gradient"
+                            bordered
+                          />
+                          <Grid.Container css={{ pl: "$6" }}>
+                            <Grid xs={12}>
+                              <Text h4 css={{ lineHeight: "$xs" }}>
+                                practice english
+                              </Text>
+                            </Grid>
+                            <Grid xs={12}>
+                              <Text css={{ color: "$accents8" }}>user</Text>
+                            </Grid>
+                          </Grid.Container>
+                        </Card.Header>
+                        <Card.Body css={{ py: "$2" }}>
+                          <Text>
+                            {_id === boldElement && isBold === true ? (
+                              <b>{text}</b>
+                            ) : (
+                              text
+                            )}
+                          </Text>
+                        </Card.Body>
+                        <Card.Footer
+                          css={{
+                            bgBlur: "#0f111466",
+                            borderTop: "$borderWeights$light solid $gray800",
+                            zIndex: 1,
+                            h: "4rem",
+                          }}
+                        >
+                          <Col>
+                            <span className="todo-btns">
+                              <DeleteTodo todoId={_id} />
+                              <UpdateTodo todoId={_id} text={text} />
+                              <button
+                                className="star-icon"
+                                onClick={() => starHandler(_id)}
+                              >
+                                <AiFillStar />
+                              </button>
+                            </span>
+                          </Col>
+                          <Col>
+                            {time !== null && (
+                              <Timer
+                                timeValue={time}
+                                dateValue={date}
+                                todoId={_id}
+                                colorHandler={statusColor}
+                              />
+                            )}
+                          </Col>
+                        </Card.Footer>
+                      </Card>
+                    </Grid>
+                  );
+                })}
+            </Grid.Container>
+          )}
         </section>
       )}
     </section>
