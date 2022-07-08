aebf8dafdcf4b873bbb6da4ec9770f8bb9114d33
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-08 18:45:07 +0430

    Send todo with user

--- client/src/components/AddTodo.js
+++ client/src/components/AddTodo.js
@@ -1,16 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { nanoid } from "nanoid";
 import { createTodo } from "../api/todoApi";
 import Filters from "./Filters";
 import NewTodoForm from "./NewTodoForm";
+import jwt_decode from "jwt-decode";
 
 const AddTodo = ({ statusHandler, searchHandler }) => {
   const navigate = useNavigate();
   const [callStatus, setCallStatus] = useState(false);
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    const jwtToken = localStorage.getItem("access_key");
+    const userInfo = jwt_decode(jwtToken);
+    setUser(userInfo.username);
+  }, [user]);
 
   const newTodo = (todoText, todotime) => {
+    console.log(user);
     const setNewTodo = {
+      user,
       id: nanoid(),
       text: todoText,
       time: todotime,
