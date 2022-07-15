972b5f598334b30940773bb70d2c1596580cf1cd
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-15 23:26:28 +0430

    Change something

--- client/src/components/container.js
+++ client/src/components/container.js
@@ -1,29 +1,51 @@
 import TodoList from "./TodoList";
 import AddTodo from "./AddTodo";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Loading, Grid } from "@nextui-org/react";
+import jwt_decode from "jwt-decode";
+import HomePage from "./HomePage";
 
 const Container = ({ searchHandler, searchText }) => {
   const [callStatus, setCallStatus] = useState(false);
-
   const statusHandler = (status) => setCallStatus(status);
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const jwtToken = localStorage.getItem("access_key");
+    jwtToken && setIsLogin(true);
+    if (jwtToken) {
+      const userInfo = jwt_decode(jwtToken);
+      const expire = Date.now() >= userInfo.exp * 1000 ? false : true;
+      expire && setIsLogin(true);
+    }
+  }, []);
 
   return (
-    <section className="main-page p-sm-5 p-1">
-      <section className="todo-container flex-sm-nowrap m-auto m-sm-0">
-        <AddTodo statusHandler={statusHandler} searchHandler={searchHandler} />
-      </section>
-      <section>
-        {callStatus ? (
-          <Grid>
-            <Loading color="secondary" textColor="secondary">
-              Please wait
-            </Loading>
-          </Grid>
-        ) : (
-          <TodoList searchText={searchText} />
-        )}
-      </section>
+    <section>
+      {!isLogin ? (
+        <HomePage />
+      ) : (
+        <section className="main-page p-sm-5 p-1">
+          {" "}
+          <section className="todo-container flex-sm-nowrap m-auto m-sm-0">
+            <AddTodo
+              statusHandler={statusHandler}
+              searchHandler={searchHandler}
+            />
+          </section>
+          <section>
+            {callStatus ? (
+              <Grid>
+                <Loading color="secondary" textColor="secondary">
+                  Please wait
+                </Loading>
+              </Grid>
+            ) : (
+              <TodoList searchText={searchText} />
+            )}
+          </section>
+        </section>
+      )}
     </section>
   );
 };
