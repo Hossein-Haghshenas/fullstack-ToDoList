17dbd9fea2c83346642aa4564d9474bcfdfb27c5
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-15 23:26:11 +0430

    Create footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="d-flex w-100 justify-content-center text-white">
+      <p>Made With ❤️ by HosseinDeveloper</p>
+    </footer>
+  );
+}
+
+export default Footer;
